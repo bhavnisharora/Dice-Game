b9b08acd8f4485d4167b113658e0e075a64f1283
bhavnisharora <bhavnisharora786@gmail.com>
2024-06-08 10:09:50 +0530

    styled-components-setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
 
+const Button = styled.button`
+background-color: black;
+color: white;
+padding: 10px 20px;
+border: none;
+`;
 
 function App() {
 
   return (
     <>
-      hello
+      <Button>Styled button</Button>
     </>
   )
 }
