19bf898a7987962d9593b5ad7917afdd331deb6f
bhavnisharora <bhavnisharora786@gmail.com>
2024-06-29 22:44:00 +0530

    start game responsive

--- src/components/StartGame.jsx
+++ src/components/StartGame.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { Button } from '../styled/button';
 const StartGame = ({ toggle }) => {
     return (
-        <Container>
+        <Container >
             <div>
                 <img src="/images/dice.png" />
             </div>
@@ -24,10 +24,31 @@ const Container = styled.div
     align-items: center;
     margin: 0 auto;
     
+    @media (max-width: 768px) {
+    flex-direction: column;
+}   
+
+     img {
+        max-width: 100%;
+        height: auto;
+    }
+
+    .content{
+    text-align: center;
+    }
+
     .content h1{
         font-size: 76px;
         margin-bottom: 20px;
         white-space: nowrap;
+
+        @media (max-width: 768px) {
+            font-size: 56px;
+        }
+
+        @media (min-width: 1024px) {
+            font-size: 76px;
+        }
     }
     
     `;
